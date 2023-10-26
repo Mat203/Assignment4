db3754a192b3f779bf2a483f2faf1aa22688b3b1
Mat_Tol <matvey.talalaevskiy@gmail.com>
2023-10-26 16:32:38 +0300

    Class CaesarCipher

--- main.cpp
+++ main.cpp
@@ -62,28 +62,67 @@ public:
     }
 };
 
+class CaesarCipher
+{
+private:
+    typedef char *(__cdecl *encryptFunc)(char *, int);
+    typedef char *(__cdecl *decryptFunc)(char *, int);
+
+    HINSTANCE handle;
+    encryptFunc encrypt_ptr;
+    decryptFunc decrypt_ptr;
+
+public:
+    CaesarCipher(const char* dllPath)
+    {
+        handle = LoadLibrary(TEXT(dllPath));
+        if (handle == nullptr || handle == INVALID_HANDLE_VALUE)
+        {
+            std::cout << "Lib not found" << std::endl;
+            throw std::runtime_error("Lib not found");
+        }
+
+        encrypt_ptr = (encryptFunc)GetProcAddress(handle, TEXT("encrypt"));
+        if (encrypt_ptr == nullptr)
+        {
+            std::cout << "Function not found" << std::endl;
+            throw std::runtime_error("Function not found");
+        }
+
+        decrypt_ptr = (decryptFunc)GetProcAddress(handle, TEXT("decrypt"));
+        if (decrypt_ptr == nullptr)
+        {
+            std::cout << "Function not found" << std::endl;
+            throw std::runtime_error("Function not found");
+        }
+    }
+
+    ~CaesarCipher()
+    {
+        FreeLibrary(handle);
+    }
+
+    string encryptString(const string& content, int key)
+    {
+        char* encryptedContent = (*encrypt_ptr)(const_cast<char*>(content.c_str()), key);
+        string result(encryptedContent);
+        delete[] encryptedContent;
+        return result;
+    }
+
+    string decryptString(const string& content, int key)
+    {
+        char* decryptedContent = (*decrypt_ptr)(const_cast<char*>(content.c_str()), key);
+        string result(decryptedContent);
+        delete[] decryptedContent;
+        return result;
+    }
+};
+
+
 int main()
 {
-    HINSTANCE handle = LoadLibrary(TEXT("library.dll"));
-    if (handle == nullptr || handle == INVALID_HANDLE_VALUE)
-    {
-        std::cout << "Lib not found" << std::endl;
-        return 1;
-    }
-
-    encrypt encrypt_ptr = (encrypt)GetProcAddress(handle, TEXT("encrypt"));
-    if (encrypt_ptr == nullptr)
-    {
-        std::cout << "Function not found" << std::endl;
-        return 1;
-    }
-
-    decrypt decrypt_ptr = (decrypt)GetProcAddress(handle, TEXT("decrypt"));
-    if (decrypt_ptr == nullptr)
-    {
-        std::cout << "Function not found" << std::endl;
-        return 1;
-    }
+    CaesarCipher cipher("library.dll");
 
     srand(time(0));
 
@@ -91,15 +130,12 @@ int main()
     int mode;
     cin >> mode;
     cin.ignore();
-
     cout << "Enter input file path: ";
     string inputPath;
     getline(cin, inputPath);
-
     cout << "Enter output file path: ";
     string outputPath;
     getline(cin, outputPath);
-
     string operation;
     int key;
 
@@ -107,7 +143,6 @@ int main()
     {
         cout << "Choose operation (encrypt or decrypt): ";
         getline(cin, operation);
-
         cout << "Enter the key: ";
         cin >> key;
         cin.ignore();
@@ -118,29 +153,36 @@ int main()
         key = rand();
         cout << "Generated key: " << key << endl;
     }
-
+    
     IReader *reader = new FileReader(inputPath);
+    
     string content = reader->read();
-
+    
     if (!content.empty())
     {
-        char *processedContent;
-
+        string processedContent;
+        
         key = key % 128;
 
         if (operation == "encrypt")
-            processedContent = (*encrypt_ptr)(const_cast<char *>(content.c_str()), key);
+            processedContent = cipher.encryptString(content, key);
         else
-            processedContent = (*decrypt_ptr)(const_cast<char *>(content.c_str()), key);
+            processedContent = cipher.decryptString(content, key);
 
+        
         IWriter *writer = new FileWriter(outputPath);
+        
         writer->write(processedContent);
 
-        delete[] processedContent;
+        
         delete writer;
-    }
+        
+     }
+    
+     delete reader;
 
-    delete reader;
+     return 0;
 
-    return 0;
 }
+
+
